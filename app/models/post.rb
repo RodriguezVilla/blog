7c9d9688c467febf10c99fa5e9ddfd8d853a9319
Ruben <ruben.rodriguez.villa@gmail.com>
2018-03-12 18:09:54 +0100

    OK Corregiso error de subida de archivos

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-
+mount_uploader :img_destacada_url, FotoUploader
     validates :titulo, :descripticion, presence: true
     validates :titulo, uniqueness: true
   #  validates :img_destacada_url, allow_blank: true, format: {
